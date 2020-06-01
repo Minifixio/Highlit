d6bb16f2b51d9932f141cb08ff2bac50ae763aed
Minifixio <emile@le-gallic.com>
2020-06-01 18:50:49 +0200

    changes : new docker config including pm2

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:12
+
+RUN npm install pm2 -g
+
+COPY server /home/node/app/
+
+RUN cd /home/node/app && npm install && mkdir logs
+
+CMD ["pm2-runtime", "build/app.js"]
--- server/build/app.d.ts
+++ server/build/app.d.ts
@@ -1,8 +1,3 @@
 export declare const app: import("express-serve-static-core").Express;
 export declare const serverMaintenance = true;
 export declare const appMaintenance = false;
-/**http.listen(3000, () => {
-    socketManager = require("./socket_manager.js");
-    socketManager.startSockets(http);
-    logger.debug('App listening on port 3000');
-});**/
